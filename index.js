9eb6d906c15b466f82ab37c6a69cfb0bfab1f2de
AlexRod <oa.defrag@gmail.com>
2023-12-19 16:05:27 +0000

    Fase 1 al 66%

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fase1 = new Fase1;
 const circulo = new Circulo();
 let enemigos = [];
 let fase = 1;
-let musicaFase2Reproducida = false;
+// let musicaFase2Reproducida = false;
 let juegoTerminado = false;
 
 // -----------------------
@@ -48,11 +48,6 @@ function draw() {
 // -----------------------
 function fase2() {
     canvas.style.backgroundImage = "url('assets/imgs/fondo.jpg')";
-    if (!musicaFase2Reproducida) {
-        playFase2Sound();
-        // Marca la música como reproducida para evitar repeticiones
-        musicaFase2Reproducida = true;
-    }
     circulo.dibujar();
     // -----------------------
     for (const enem of enemigos) {
@@ -81,14 +76,51 @@ function colission() {
         }
     }
 }
-function playFase2Sound() {
-    let fase2Sound = new Audio('./assets/sounds/fase2.mp3');
-    fase2Sound.play();
-}
+// function playFase2Sound() {
+//     let fase2Sound = new Audio('./assets/sounds/fase2.mp3');
+//     fase2Sound.play();
+// }
 function playColisionSound() {
     let colisionSound = new Audio('./assets/sounds/pop.mp3');
     colisionSound.play();
 }
+
+function musicaJuego() {
+    let fase1Sound = new Audio('./assets/sounds/fase1.mp3');
+    let fase2Sound = new Audio('./assets/sounds/fase2.mp3');
+    let fase3Sound = new Audio('./assets/sounds/fase3.mp3');
+
+    if (fase == 1) {
+        // Detener otras pistas de sonido si están reproduciéndose
+        fase2Sound.pause();
+        fase2Sound.currentTime = 0;
+        fase3Sound.pause();
+        fase3Sound.currentTime = 0;
+
+        // Reproducir la pista de sonido de la fase 1
+        fase1Sound.play();
+    } else if (fase == 2) {
+        // Detener otras pistas de sonido si están reproduciéndose
+        fase1Sound.pause();
+        fase1Sound.currentTime = 0;
+        fase3Sound.pause();
+        fase3Sound.currentTime = 0;
+
+        // Reproducir la pista de sonido de la fase 2
+        fase2Sound.play();
+    } else {
+        // Detener otras pistas de sonido si están reproduciéndose
+        fase1Sound.pause();
+        fase1Sound.currentTime = 0;
+        fase2Sound.pause();
+        fase2Sound.currentTime = 0;
+
+        // Reproducir la pista de sonido de la fase 3
+        fase3Sound.play();
+    }
+}
+
 // -----------------------
 // -----------------------
+musicaJuego();
 draw();
