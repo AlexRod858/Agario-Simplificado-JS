d1cf8a75fc5e0cd4cea49dc65e8e6a3dad039b7b
AlexRod <oa.defrag@gmail.com>
2023-12-27 12:07:30 +0000

    Nueva pantalla 1 terminada

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-import Fase2 from "./classes/fase2.js";
 // -----------------------
 const canvas = document.getElementById('canvas');
 const start = document.getElementById('start');
 const gameover = document.getElementById('gameover');
-canvas.width = 1080;
-canvas.height = 760;
-const ctx = canvas.getContext('2d');
 // -----------------------
 let musicaFondo = new Audio('./assets/sounds/fase1.mp3');
+musicaFondo.play();
+let clickSound = new Audio('./assets/sounds/clickDentro.mp3');
 // -----------------------
-const fase2 = new Fase2();
 // -----------------------
 // -----------------------
-function startGame(){
-start.style.display = 'none';
-canvas.style.display = 'none';
-gameover.style.display = 'block';
-    // juego();
+const startButton = document.querySelector('#start a');
+startButton.addEventListener('click', startGame);
+function startGame() {
+    clickSound.play();
+    start.style.display = 'none';
+    canvas.style.display = 'block';
+    gameover.style.display = 'none';
 }
-
-
 
 // -----------------------
 // -----------------------
 // F U N C I O N  P R I N C I P A L
 // -----------------------
 // -----------------------
+import Fase2 from "./classes/fase2.js";
+canvas.width = 1080;
+canvas.height = 760;
+const ctx = canvas.getContext('2d');
+const fase2 = new Fase2();
 
-// function juego() {
-//     requestAnimationFrame(juego);
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+function canvasScreen() {
+    requestAnimationFrame(canvasScreen);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-//     fase2.dibujarFondo();
-//     fase2.pintarHero();
-//     fase2.pintarEnemigos();
-//     // fase2.temporizador();
+    fase2.dibujarFondo();
+    fase2.pintarHero();
+    fase2.pintarEnemigos();
+    // fase2.temporizador();
 
-// }
-startGame();
+}
+canvasScreen();
